1890343875d9b54afa5eaa9d24c3a25393f2b850
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-03-08 00:48:27 +0700

    Ex3 finalize

---
+++ W5_EX3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+class CPhanSo 
+{
+    private:
+        int tu;
+        int mau;
+    public:
+        ~CPhanSo();//phuong thuc pha huy (destructor)
+};
