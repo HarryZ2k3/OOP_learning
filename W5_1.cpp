662c09b945160e777114565cff70a98e4db0a3ac
Harry PhuongDai <phuongdai.saigon@gmail.com>
2022-03-08 00:32:47 +0700

    week 5_1 commit

---
+++ W5_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class CPhanSo
+{
+    private:
+        int tu;
+        int mau;
+    public:
+    CPhanSo(); // phuong thuc thietlap mac dinh (default constructor)
+    CPhanSo(int, int); // phuong thuc thiet lap khi biet thong tin (constructor)
+    CPhanSo(const CPhanSo&); // Phuong thuc thiet lap sao chep
+};
+
+CPhanSo::CPhanSo()
+{
+    int = 0;
+    int = 1;
+}
+
+CPhanSo::CPhanSO(int &a, int &b)
+{
+    
+}
